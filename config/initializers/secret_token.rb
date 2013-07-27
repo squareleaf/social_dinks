1e8bab0dc46ce50347c01b7b0a9179aaaba0fc71
tiffanyg <tiffany@binarysprocket.com>
2013-07-26 21:10:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinks::Application.config.secret_token = '0fe0fdba67009c6bf2f3a7611e2b2e3ffc4d6d4db9d79cc5d85ad7a84500919ab5765ea68b432efe6f82c84d54fc9f3059ce246d572b4812701ed6570c2c7c94'
